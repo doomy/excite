58a14650a67e86f321bb8339a921258a7d6d80fd
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2018-11-28 14:33:29 +0100

    Initial commit

---
+++ js/layout.js
@@ -0,0 +1,16 @@
+$(function() {
+   $(".background").addClass("opacity50");
+   $(".header-background").addClass("opacity80");
+   $("nav").addClass("fullOpacity");
+   $(".content").addClass("fullOpacity");
+   setTimeout(function() {
+      $(".header-background").css("background-image", "url(img/slideshow/002.jpg)");
+   }, 4000);
+    setTimeout(function() {
+        $(".header-background").css("background-image", "url(img/slideshow/001.jpg)");
+    }, 10000);
+});
+
+$(window).scroll(function() {
+   $("nav").addClass("scrolled");
+});
