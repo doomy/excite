a1b0b7894daa4a58a1c8bde9c1e6aeafddcaa1a9
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2018-11-28 19:51:18 +0100

    Remove commented out code

--- js/layout.js
+++ js/layout.js
@@ -3,12 +3,6 @@ $(function() {
    $(".header-background").addClass("opacity80");
    $("nav").addClass("fullOpacity");
    $(".content").addClass("fullOpacity");
-   /*setTimeout(function() {
-      $(".header-background").css("background-image", "url(img/slideshow/002.jpg)");
-   }, 4000);
-    setTimeout(function() {
-        $(".header-background").css("background-image", "url(img/slideshow/001.jpg)");
-    }, 10000);*/
 });
 
 $(window).scroll(function() {
