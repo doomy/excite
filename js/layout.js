28cdff1fb65b67682756421c424683f4a922dd80
vladimir.bartek@gmail.com <vladimir.bartek@gmail.com>
2018-12-01 17:51:11 +0100

    Subpages mechanism

--- js/layout.js
+++ js/layout.js
@@ -1,8 +1,20 @@
 $(function() {
+    $('.content').hide();
+    $('.content').first().show();
    $(".background").addClass("opacity50");
    $(".header-background").addClass("fullOpacity");
    $("nav").addClass("fullOpacity");
    $(".content").addClass("fullOpacity");
+
+   $('nav ul li a').click(function() {
+      var selector = $(this).attr("href");
+      $('.content').hide();
+       $(selector).addClass("invisible");
+      $(selector).show();
+
+      $(selector).removeClass('invisible');
+      return false;
+   })
 });
 
 $(window).scroll(function() {
